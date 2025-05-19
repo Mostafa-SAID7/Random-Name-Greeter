c44abdade0f1011146e77aa4b761d7d1755759c0
Mostafa Said <m.ssaid356@gmail.com>
2025-05-20 00:54:27 +0300

    Add files via upload

---
+++ newElsokary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Elsokary
+{
+    public class Program
+    {
+        // Declare Random once to avoid same seed issue
+        static Random rnd = new Random();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Random Greetings:\n");
+
+            for (int i = 0; i < 5; i++) // Print 5 random greetings
+            {
+                string name = GetRandomName();
+                Console.WriteLine("Hello " + name);
+            }
+        }
+
+        public static string GetRandomName()
+        {
+            string[] names = { "Mostafa", "Ali", "Sara", "John", "Laila", "Ahmed", "Nora" };
+            int index = rnd.Next(names.Length);
+            return names[index];
+        }
+    }
+}
